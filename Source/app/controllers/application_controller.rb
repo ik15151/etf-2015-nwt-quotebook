162e6740795309e0c531884bca86e00ec2229772
ik15151 <ik15151@etf.unsa.ba>
2015-05-22 21:06:17 +0200

    Testiram Heroku-GitHub

--- Source/app/controllers/application_controller.rb
+++ Source/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Testiram još jednom."
+    render text: "Hello, world!"
   end
 
 end
