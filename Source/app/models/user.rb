dee42202eb2d1204f4f31d74c62485cc2268a1ec
ik15151 <ik15151@etf.unsa.ba>
2015-05-27 12:00:25 +0200

    Length validation

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
   belongs_to :privilege
   has_many :password_recovery_tokens
   has_many :categories
