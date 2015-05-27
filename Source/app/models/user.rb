243edc54d20bc14ca221ad48c815c45d2b781f3b
ik15151 <ik15151@etf.unsa.ba>
2015-05-27 11:52:17 +0200

    Validating presence

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
   belongs_to :privilege
   has_many :password_recovery_tokens
   has_many :categories
