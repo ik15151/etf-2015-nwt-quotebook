142077568780fe9890d9a0c6256822886ccf87bf
ik15151 <ik15151@etf.unsa.ba>
2015-05-28 20:21:09 +0200

    A hashed password

--- Source/db/schema.rb
+++ Source/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528165819) do
+ActiveRecord::Schema.define(version: 20150528181313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,14 +81,15 @@ ActiveRecord::Schema.define(version: 20150528165819) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "username",     null: false
-    t.string   "email",        null: false
-    t.string   "password",     null: false
-    t.string   "salt",         null: false
+    t.string   "name",            null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password",        null: false
+    t.string   "salt",            null: false
     t.integer  "privilege_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
