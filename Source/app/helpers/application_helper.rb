1c390df9674e43fb4bb15b14063ed205bc17ced5
ik15151 <ik15151@etf.unsa.ba>
2015-05-30 15:11:43 +0200

    Editujem naslov (base_title)

--- Source/app/helpers/application_helper.rb
+++ Source/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Quotebook - notebook for quotes"
+    base_title = "Quotebook"
     if page_title.empty?
       base_title
     else
