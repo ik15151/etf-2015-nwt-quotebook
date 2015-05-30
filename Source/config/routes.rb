9e0153f762dff4d01999739c3c48b0d34fc6cc18
ik15151 <ik15151@etf.unsa.ba>
2015-05-30 11:06:32 +0200

    A Users resource

--- Source/config/routes.rb
+++ Source/config/routes.rb
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
+  resources :users
   resources :authors
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
