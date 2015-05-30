67f50e840b6517d028c3693472f4d6efe960c0af
ik15151 <ik15151@etf.unsa.ba>
2015-05-30 18:57:22 +0200

    Successful signups: The flash

--- Source/app/controllers/users_controller.rb
+++ Source/app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Quotebook!"
       redirect_to @user # equivalent: redirect_to user_url(@user)
     else
       render 'new'
