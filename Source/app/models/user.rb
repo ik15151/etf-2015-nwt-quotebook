c7bb3ed087a66fc80eb4d43cc45d4175782f9670
ik15151 <ik15151@etf.unsa.ba>
2015-05-31 21:27:45 +0200

    Remember me: Remember token and digest

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token
+  
+  before_save { self.email = email.downcase } # without self the assignment would create a local variable
+  
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
+  
   validates :password, presence: true, length: { minimum: 6 }
+  
+  has_secure_password
+  
   has_many :categories
   
   # Returns the hash digest of the given string.
@@ -16,4 +22,16 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
   
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token 
+    # without self the assignment would create a local variable called remember_token, which isn’t what we want
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+    
 end
