b6007803253582f8399e04d8dcfa3a3bbdd6f7cb
ik15151 <ik15151@etf.unsa.ba>
2015-06-02 21:06:03 +0200

    Dodajem zaboravljeni "reset_sent_at" atribut

--- Source/db/schema.rb
+++ Source/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602170909) do
+ActiveRecord::Schema.define(version: 20150602190142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150602170909) do
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
