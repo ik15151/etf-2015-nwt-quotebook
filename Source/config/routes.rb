4401bfa5b24f4d51a1a77ff3ac82f6a7ebf112f2
ik15151 <ik15151@etf.unsa.ba>
2015-06-06 15:38:58 +0200

    Routes for the Quotes resource

--- Source/config/routes.rb
+++ Source/config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :quotes,          only: [:create, :destroy]
   resources :authors
   
   # Example of regular route:
