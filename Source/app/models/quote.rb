8746e4d4128a659b6e0b9b7a5731cc3aff000f19
ik15151 <ik15151@etf.unsa.ba>
2015-06-06 11:34:31 +0200

    Accessible attributes and the first validation

--- Source/app/models/quote.rb
+++ Source/app/models/quote.rb
@@ -1,4 +1,10 @@
 class Quote < ActiveRecord::Base
+  belongs_to :user  
   belongs_to :author
   has_and_belongs_to_many :tags
+  
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 3000 }
+  validates :source, length: { maximum: 200 }
+  validates :comment, length: { maximum: 5000 }  
 end
