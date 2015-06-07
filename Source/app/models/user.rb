5d57c4df162e1d0ec04858821b65c5f98bb65924
ik15151 <ik15151@etf.unsa.ba>
2015-06-07 14:32:56 +0200

    User/relationship associations

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -15,6 +15,9 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :quotes, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
   
   # Returns the hash digest of the given string.
   def User.digest(string)
