6f2b4b7e3843caeca7c7ddf95f57c56bc8a11860
ik15151 <ik15151@etf.unsa.ba>
2015-06-07 17:23:38 +0200

    The final implementation of the feed

--- Source/app/models/user.rb
+++ Source/app/models/user.rb
@@ -85,7 +85,10 @@ class User < ActiveRecord::Base
   
   # Returns a user's status feed.
   def feed
-    Quote.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Quote.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
   # Follows a user.
