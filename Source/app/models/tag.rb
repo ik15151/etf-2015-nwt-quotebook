54f50e1262b7016be96dc7632f200472a8491b4b
ik15151 <ik15151@etf.unsa.ba>
2015-06-15 21:07:00 +0200

    Tags cloud, elhamdulillah

--- Source/app/models/tag.rb
+++ Source/app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :quotes, through: :taggings
+  
+  def self.counts
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id", "tags.name")
+  end
 end
