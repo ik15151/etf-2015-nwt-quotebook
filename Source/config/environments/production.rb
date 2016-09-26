15ff0f2e06fdb9e6ef0262222bca6512e50e08f2
ik15151 <ik15151@etf.unsa.ba>
2016-09-26 14:32:46 +0200

    Izmjena passworda za gmail

--- Source/config/environments/production.rb
+++ Source/config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'etfqbook',
-    :password       => 'etfqbookpass',
+    :password       => 'etfqbookpassword',
     :domain         => 'gmail.com',
     :enable_starttls_auto => true
   } 
