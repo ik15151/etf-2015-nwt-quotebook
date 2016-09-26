f516231a041f4bcd3f2ae474d7afcc7c798d24a6
ik15151 <ik15151@etf.unsa.ba>
2016-09-26 15:35:09 +0200

    Gmail in production (enable_starttls_auto = false)

--- Source/config/environments/production.rb
+++ Source/config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
     :user_name      => 'etfqbook',
     :password       => 'etfqbookpassword',
     :domain         => 'gmail.com',
-    :enable_starttls_auto => true
+    :enable_starttls_auto => false
   } 
   
 
