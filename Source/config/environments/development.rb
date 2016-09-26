8a5be695bb6af01f71fe3cc383901e8251abfa0c
ik15151 <ik15151@etf.unsa.ba>
2016-09-26 15:29:23 +0200

    Gmail u developmentu (radi)

--- Source/config/environments/development.rb
+++ Source/config/environments/development.rb
@@ -39,6 +39,20 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'etfqbook',
+    :password       => 'etfqbookpassword',
+    :domain         => 'gmail.com',
+    :enable_starttls_auto => true
+  } 
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
